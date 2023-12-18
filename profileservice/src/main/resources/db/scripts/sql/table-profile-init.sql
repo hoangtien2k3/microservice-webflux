421d66d32adbf1d61a7acd7b8ca422bfc5ae4cb3
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-12-19 00:55:30 +0700

    liquibase

---
+++ profileservice/src/main/resources/db/scripts/sql/table-profile-init.sql
@@ -0,0 +1,3 @@
+INSERT INTO profile (id, email, status, name, role) VALUES
+(1, 'dev@gmail.com', 'ACTIVE', 'Hoàng Anh Tiến', 'DEVELOPER'),
+(2, 'customer@gmail.com', 'ACTIVE', 'Nguyễn Văn Khánh', 'CUSTOMER');
