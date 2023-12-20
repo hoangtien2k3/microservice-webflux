4091052b8d3d168df8d13837d5ea10561bcbb45a
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-12-20 17:21:29 +0700

    docker-compose

--- accountservice/src/main/java/com/hoangtien2k3/AccountserviceApplication.java
+++ accountservice/src/main/java/com/hoangtien2k3/AccountserviceApplication.java
@@ -2,10 +2,12 @@ package com.hoangtien2k3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
+@EnableDiscoveryClient
 public class AccountserviceApplication {
 
 	public static void main(String[] args) {
--- profileservice/src/main/java/com/hoangtien2k3/ProfileserviceApplication.java
+++ profileservice/src/main/java/com/hoangtien2k3/ProfileserviceApplication.java
@@ -2,11 +2,13 @@ package com.hoangtien2k3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
+@EnableDiscoveryClient
 //@ComponentScan({"com.hoangtien2k3.profileservice", "com.hoangtien2k3.commonservice"})
 public class ProfileserviceApplication {
 
